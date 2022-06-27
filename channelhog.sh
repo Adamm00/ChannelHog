dbb2c8a15b357e8681a424b60880e8912aa273d3
Adamm <28355318+Adamm00@users.noreply.github.com>
2022-06-27 18:54:43 +1000

    Remove model check

--- channelhog.sh
+++ channelhog.sh
@@ -283,88 +283,83 @@ EOF
 	;;
 
 	install)
-		[ -z "$(nvram get odmpid)" ] && model="$(nvram get productid)" || model="$(nvram get odmpid)"
-		if [ "$model" = "RT-AX88U" ] || [ "$model" = "RT-AX86U" ] || [ "$model" = "RT-AX58U" ]; then
-			Check_Lock "$@"
-			while true; do
-				echo "Would You Like To Enable Discord Notifications?"
-				echo "[1]  --> Yes"
-				echo "[2]  --> No"
-				echo
-				echo "[e]  --> Exit Menu"
-				echo
-				echo "Please Select Option"
-				printf "[1-2]: "
-				read -r "menu1"
-				echo
-				case "$menu1" in
-					1)
-						while true; do
-							echo "Please Enter Discord Channel Webhook URL"
-							printf "[URL]: "
-							read -r "webhookurl"
-							echo
-							if [ "$webhookurl" = "e" ]; then
-								echo "[*] Exiting!"
-								echo; exit 0
-							fi
-							if ! curl -sI "$webhookurl" | grep -qE "HTTP/1.[01] [23].." || ! curl -s "$webhookurl" | grep -qF "token"; then
-								echo "[*] $webhookurl Isn't A Valid URL!"
-								echo
-								continue
-							fi
-							enablediscord="true"
-							break
-						done
-						break
-					;;
-					2)
-						echo "[i] Discord Notifications Disabled"
-						enablediscord="false"
-						break
-					;;
-					e|exit)
-						echo "[*] Exiting!"
-						echo; exit 0
-					;;
-					*)
-						echo "[*] $menu1 Isn't An Option!"
+		Check_Lock "$@"
+		while true; do
+			echo "Would You Like To Enable Discord Notifications?"
+			echo "[1]  --> Yes"
+			echo "[2]  --> No"
+			echo
+			echo "[e]  --> Exit Menu"
+			echo
+			echo "Please Select Option"
+			printf "[1-2]: "
+			read -r "menu1"
+			echo
+			case "$menu1" in
+				1)
+					while true; do
+						echo "Please Enter Discord Channel Webhook URL"
+						printf "[URL]: "
+						read -r "webhookurl"
 						echo
-					;;
-				esac
-			done
-			if [ ! -f "/jffs/scripts/init-start" ]; then
-				echo "#!/bin/sh" > /jffs/scripts/init-start
-				echo >> /jffs/scripts/init-start
-			elif [ -f "/jffs/scripts/init-start" ] && ! head -1 /jffs/scripts/init-start | grep -qE "^#!/bin/sh"; then
-				sed -i '1s~^~#!/bin/sh\n~' /jffs/scripts/init-start
-			fi
-			cmdline="sh /jffs/addons/channelhog/channelhog.sh start # ChannelHog"
-			if grep -qE "^$cmdline" /jffs/scripts/init-start; then
-				sed -i "s~^sh /jffs/addons/channelhog/channelhog.sh .* # ChannelHog~$cmdline~" /jffs/scripts/init-start
-			else
-				echo "$cmdline" >> /jffs/scripts/init-start
-			fi
-			chmod 755 "/jffs/scripts/init-start" "/jffs/addons/channelhog/channelhog.sh"
-			if [ -d "/opt/bin" ] && [ ! -L "/opt/bin/channelhog" ]; then
-				ln -s /jffs/addons/channelhog/channelhog.sh /opt/bin/channelhog
-			fi
-			Unload_Cron
-			Load_Cron
-			Write_Config
-			if [ "$(nvram get wl1_bw)" != "5" ]; then
-				nvram set wl1_bw=5
-				echo "[i] Restarting 5GHz Radio To Complete Installation"
-				printf "[i] Press Enter To Continue..."; read -r "continue"
-				wl -i "$port5ghz1" down
-				wl -i "$port5ghz1" up
-				if [ -n "$port5ghz2" ]; then
-					wl -i "$port5ghz2" down
-					wl -i "$port5ghz2" up
-				fi
-			fi
+						if [ "$webhookurl" = "e" ]; then
+							echo "[*] Exiting!"
+							echo; exit 0
+						fi
+						if ! curl -sI "$webhookurl" | grep -qE "HTTP/1.[01] [23].." || ! curl -s "$webhookurl" | grep -qF "token"; then
+							echo "[*] $webhookurl Isn't A Valid URL!"
+							echo
+							continue
+						fi
+						enablediscord="true"
+						break
+					done
+					break
+				;;
+				2)
+					echo "[i] Discord Notifications Disabled"
+					enablediscord="false"
+					break
+				;;
+				e|exit)
+					echo "[*] Exiting!"
+					echo; exit 0
+				;;
+				*)
+					echo "[*] $menu1 Isn't An Option!"
+					echo
+				;;
+			esac
+		done
+		if [ ! -f "/jffs/scripts/init-start" ]; then
+			echo "#!/bin/sh" > /jffs/scripts/init-start
+			echo >> /jffs/scripts/init-start
+		elif [ -f "/jffs/scripts/init-start" ] && ! head -1 /jffs/scripts/init-start | grep -qE "^#!/bin/sh"; then
+			sed -i '1s~^~#!/bin/sh\n~' /jffs/scripts/init-start
+		fi
+		cmdline="sh /jffs/addons/channelhog/channelhog.sh start # ChannelHog"
+		if grep -qE "^$cmdline" /jffs/scripts/init-start; then
+			sed -i "s~^sh /jffs/addons/channelhog/channelhog.sh .* # ChannelHog~$cmdline~" /jffs/scripts/init-start
 		else
-			echo "[*] ChannelHog currently only supports the RT-AX88U / RT-AX86U / RT-AX58U"
+			echo "$cmdline" >> /jffs/scripts/init-start
+		fi
+		chmod 755 "/jffs/scripts/init-start" "/jffs/addons/channelhog/channelhog.sh"
+		if [ -d "/opt/bin" ] && [ ! -L "/opt/bin/channelhog" ]; then
+			ln -s /jffs/addons/channelhog/channelhog.sh /opt/bin/channelhog
+		fi
+		Unload_Cron
+		Load_Cron
+		Write_Config
+		if [ "$(nvram get wl1_bw)" != "5" ]; then
+			nvram set wl1_bw=5
+			echo "[i] Restarting 5GHz Radio To Complete Installation"
+			printf "[i] Press Enter To Continue..."; read -r "continue"
+			wl -i "$port5ghz1" down
+			wl -i "$port5ghz1" up
+			if [ -n "$port5ghz2" ]; then
+				wl -i "$port5ghz2" down
+				wl -i "$port5ghz2" up
+			fi
 		fi
 	;;
 
